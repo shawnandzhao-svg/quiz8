fea98ca985e76217db203d68d473ca1758b29f25
Ziqing Zhao <zhao.3518@osu.edu>
2026-04-09 21:37:18 -0400

    feat: update welcome message to v2 and enable CD

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from CI/CD Lab!', status: 'ok' });
+  res.json({  message: 'Hello from CI/CD Lab - v2!', status: 'ok' });
 });
 
 app.get('/health', (req, res) => {
